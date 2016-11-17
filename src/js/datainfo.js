3e7136b5c40f6a47f3d5825483becf1721a28afe
Carmen Chico <CarmenChico@icloud.com>
2016-11-17 12:48:00 -0500

    completed form builder

--- src/js/datainfo.js
+++ src/js/datainfo.js
@@ -1,17 +1,90 @@
 import $ from 'jquery';
 
-var API = "http://json-data.herokuapp.com/forms`"
+
+var API = "http://json-data.herokuapp.com/forms"
 function requestData () {
-    $.ajax({
+     return $.ajax({
     url: `${API}`
+
   });
 };
 
+//using the for loop
+// function extractData(data) {
+//   for (var i = 0; i<data.length; i++){
+//     var form= data(i);
+//     console.log(form);
+//   }
+// }
+
+// using higher order functions
 function extractData(data) {
-  console.log(data);
+  data.map(function(separate) {
+    // console.log(separate);
+    if (separate.type==="text"){
+          var firstHTML=  `
 
-}
-;
+                  <div class="outerbox">
+                    <p class="fa ${separate.icon} aria-hidden="true">
+                      <input type="${separate.type}" placeholder="${separate.label}">
+                    </p>
+                  </div>
 
+                   `;
+                   $(".form").append(firstHTML);
+    } else if (separate.type ==="email") {
+        var emailHTML= `
+                  <div class="outerbox">
+                    <p class="fa ${separate.icon} aria-hidden="true">
+                      <input type="${separate.type}" placeholder="${separate.label}">
+                    </p>
+                  </div>
+                  `;
+                  $(".form").append(emailHTML);
+    } else if (separate.type ==="select") {
+        var languageHTML= `
+                  <div class="outerbox">
+                    <select id="select">
+                      <option>${separate.label}...</option>
+                      <option>${separate.options[0].label}</option>
+                      <option>${separate.options[1].label}</option>
+                      <option>${separate.options[2].label}</option>
+                      <option>${separate.options[3].label}</option>
+                      <option>${separate.options[4].label}</option>
+                    </select>
+                  </div>
+                  `;
+                  console.log(separate.options);
+                  console.log(separate.options[0]);
+                    console.log(separate.options[0].label);
+                    console.log(separate.options[1].label);
+                  $(".form").append(languageHTML);
+    } else if (separate.type ==="textarea") {
+        var commentHTML= `
+                  <div class="outerbox">
+                    <p class="fa ${separate.icon} aria-hidden="true">
+                      <textarea type="${separate.type}"  cols="55" rows="5" placeholder="${separate.label}"></textarea>
+                    </p>
+                  </div>
+                  `;
+                  $(".form").append(commentHTML);
+    } else if (separate.type ==="tel") {
+        var telHTML= `
+                  <div class="outerbox">
+                    <p class="fa ${separate.icon} aria-hidden="true">
+                      <input type="${separate.type}" placeholder="${separate.label}">
+                    </p>
+                  </div>
+                  `;
+                  $(".form").append(telHTML);
+    };
+    // var icon = separate.icon;
+    // console.log(separate.icon);
+  })
 
-export { requestData, extractData };
+};
+
+//the promise is going to be pulled from the main.js file .. so its being omited here
+// requestData().then(extractData);
+
+export { requestData, extractData};
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 import $ from 'jquery';
-import { getData } from "./datainfo";
+
+import { requestData, extractData} from "./datainfo";
 
 
 requestData().then(extractData);
+
+
+// $(".formInput").append(separatedData);
+// $(".formInput").append(firstHTML);
